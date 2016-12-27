090280c0c9a581bd009b510bfaddb77d7ab59df8
just-a-bird <robin.one.half@gmail.com>
2016-12-27 08:27:32 -0500

    WIP

--- FEITS/Model/MessageBlock.cs
+++ FEITS/Model/MessageBlock.cs
@@ -15,22 +15,23 @@ namespace FEITS.Model
         public List<MessageLine> MessageLines { get; } = new List<MessageLine>();
 
         #region Parsing
+
         /// <summary>
         /// Returns a list of parsed message lines.
         /// </summary>
         /// <param name="message">The message to parse</param>
         public void ParseMessage(string message)
         {
-            message = message.Replace("\\n", "\n").Replace("$k\n", "$k\\n");
+            message = CommandTokens.NormalizeLineEndings(message);
 
             //Split the lines based on the line-end markers
-            var delimiters = new List<string> { "$k$p", "$k\\n" };
+            var delimiters = new List<string> {"$k$p", "$k\\n"};
             var pattern = "(?<=" + string.Join("|", delimiters.Select(Regex.Escape).ToArray()) + ")";
             var splits = Regex.Split(message, pattern);
 
-            foreach(var str in splits)
+            foreach (var s in splits)
             {
-                var newLine = new MessageLine {RawLine = str};
+                var newLine = new MessageLine {RawLine = s};
                 MessageLines.Add(newLine);
             }
         }
@@ -38,12 +39,32 @@ namespace FEITS.Model
         public static Tuple<string, Command> ParseCommand(string line, int offset)
         {
             var trunc = line.Substring(offset);
-            string[] NoParams = { "$Wa", "$Wc", "$a", "$Nu", "$N0", "$N1", "$k\\n", "$k", "$t0", "$t1", "$p", "$Wd", "$Wv" };
-            string[] SingleParams = { "$E", "$Sbs", "$Svp", "$Sre", "$Fw", "$Ws", "$VF", "$Ssp", "$Fo", "$VNMPID", "$Fi", "$b", "$w", "$l" };
-            string[] DoubleParams = { "$Wm", "$Sbv", "$Sbp", "$Sls", "$Slp", "$Srp" };
+            IEnumerable<string> NoParams =
+               new List<string>
+                {
+                    CommandTokens.Wa,
+                    CommandTokens.Wc,
+                    CommandTokens.a,
+                    CommandTokens.Nu,
+                    CommandTokens.N0,
+                    CommandTokens.N1,
+                    CommandTokens.kn,
+                    CommandTokens.k,
+                    CommandTokens.t0,
+                    CommandTokens.t1,
+                    CommandTokens.p,
+                    CommandTokens.Wd,
+                    CommandTokens.Wv
+                };
+            string[] SingleParams =
+            {
+                "$E", "$Sbs", "$Svp", "$Sre", "$Fw", "$Ws", "$VF", "$Ssp", "$Fo", "$VNMPID", "$Fi",
+                "$b", "$w", "$l"
+            };
+            string[] DoubleParams = {"$Wm", "$Sbv", "$Sbp", "$Sls", "$Slp", "$Srp"};
             var newCmd = new Command();
 
-            foreach(var delim in NoParams)
+            foreach (var delim in NoParams)
             {
                 if (!trunc.StartsWith(delim)) continue;
 
@@ -54,7 +75,7 @@ namespace FEITS.Model
                 break;
             }
 
-            foreach(var delim in SingleParams)
+            foreach (var delim in CommandTokens.GetSingleParamTokens())
             {
                 if (!trunc.StartsWith(delim)) continue;
 
@@ -66,7 +87,7 @@ namespace FEITS.Model
                 line = line.Substring(0, offset) + line.Substring(index + 1);
             }
 
-            foreach(var delim in DoubleParams)
+            foreach (var delim in CommandTokens.GetDoubleParamTokens())
             {
                 if (!trunc.StartsWith(delim)) continue;
 
@@ -76,27 +97,27 @@ namespace FEITS.Model
                 var index = line.IndexOf("|", offset, StringComparison.Ordinal);
                 var index2 = line.IndexOf("|", index + 1, StringComparison.Ordinal);
 
-                if(delim == "$Srp")
+                switch (delim)
                 {
-                    Console.WriteLine(@"$Srp processed!");
-                }
-
-                if(delim == "$Wm")
-                {
-                    newCmd.Params[0] = line.Substring(offset + delim.Length, index - (offset + delim.Length));
-                    newCmd.Params[1] = line.Substring(index + 1, 1);
-                    line = line.Substring(0, offset) + line.Substring(index + 2);
-                }
-                else
-                {
-                    newCmd.Params[0] = line.Substring(offset + delim.Length, index - (offset + delim.Length));
-                    newCmd.Params[1] = line.Substring(index + 1, index2 - (index + 1));
-                    line = line.Substring(0, offset) + line.Substring(index2 + 1);
+                    case CommandTokens.Srp:
+                        Console.WriteLine(@"$Srp processed!");
+                        break;
+                    case CommandTokens.Wm:
+                        newCmd.Params[0] = line.Substring(offset + delim.Length, index - (offset + delim.Length));
+                        newCmd.Params[1] = line.Substring(index + 1, 1);
+                        line = line.Substring(0, offset) + line.Substring(index + 2);
+                        break;
+                    default:
+                        newCmd.Params[0] = line.Substring(offset + delim.Length, index - (offset + delim.Length));
+                        newCmd.Params[1] = line.Substring(index + 1, index2 - (index + 1));
+                        line = line.Substring(0, offset) + line.Substring(index2 + 1);
+                        break;
                 }
             }
 
             return new Tuple<string, Command>(line, newCmd);
         }
+
         #endregion
 
         public string CompileMessage(bool includePrefix = true)
